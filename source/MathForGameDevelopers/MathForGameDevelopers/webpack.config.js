02797b03f0e81d9403f8e42432a297161d6c3d91
bricknose <me@bricknose.com>
2019-06-11 19:37:04 -0500

    Move files around.

---
+++ source/MathForGameDevelopers/MathForGameDevelopers/webpack.config.js
@@ -0,0 +1,31 @@
+﻿module.exports = {
+    entry: {
+        'app': "./src/app.ts",
+        'app.min': "./src/app.ts"
+    },
+    output: {
+        path: path.resolve(__dirname, "_bundles"),
+            filename: "[name].js",
+            libraryTarget: "umd",
+            library: "MyLib",
+            umdNamedDefine: true
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    devtool: "source-map",
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin({
+                minimize: true,
+                sourceMap: true,
+                include: /\.min\.js$/
+            })
+        ],
+        module: {
+        loaders: [{
+            test: /\.tsx?$/,
+            loader: "ts-loader",
+            exclude: /node_modules/
+        }]
+    }
+};
