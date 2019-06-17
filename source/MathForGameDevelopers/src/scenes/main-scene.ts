a3cbdfe4285d068ac188de3c916ac855a846153e
bricknose <me@bricknose.com>
2019-06-17 04:17:50 -0500

    Clean up main scene.

--- source/MathForGameDevelopers/src/scenes/main-scene.ts
+++ source/MathForGameDevelopers/src/scenes/main-scene.ts
@@ -6,7 +6,6 @@ import { Controls } from "../libraries/controls";
 import { Point } from "../libraries/math";
 
 export class MainGameScene extends Phaser.Scene {
-    private background: Phaser.GameObjects.Image;
     private player: Pacman;
 
     constructor() {
@@ -16,10 +15,6 @@ export class MainGameScene extends Phaser.Scene {
     }
 
     preload(): void {
-        this.load.image(
-            "background",
-            "assets/background.png"
-        );
         this.load.spritesheet("pacman-sheet", "assets/pacman.png",
             {
                 frameWidth: 13,
@@ -31,17 +26,9 @@ export class MainGameScene extends Phaser.Scene {
     }
 
     create(): void {
-        // create background
-        //this.background = this.add.image(0, 0, "background");
-        //this.background.setOrigin(0, 0);
-        //this.background.setDisplaySize(this.sys.canvas.width, this.sys.canvas.height);
-
-        //this.physics.add.sprite(50, 50, "pacman-sheet", 0);
-
         let controls = new Controls(this.input.keyboard);
 
         this.player = new Pacman(this, new Point(20, 20), controls);
-        //this.physics.add.existing(this.player);
         this.add.existing(this.player);
     }
 
